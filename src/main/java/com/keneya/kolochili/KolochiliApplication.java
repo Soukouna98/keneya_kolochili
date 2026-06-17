2946139372c9415f1d9fb7e99390eca576a3fed5
Amadou Oumar N'diaye <amadou14112004@gmail.com>
2026-06-17 14:57:22 +0000

    init

---
+++ src/main/java/com/keneya/kolochili/KolochiliApplication.java
@@ -0,0 +1,13 @@
+package com.keneya.kolochili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolochiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolochiliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keneya/kolochili/KolochiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keneya.kolochili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolochiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
